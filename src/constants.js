ef98cf09459a12367b51a262894c0b4e5593564d
YMS <yms1789@naver.com>
2022-11-11 17:14:03 +0900

    Feat: 메세지 및 상수 값을 저장하기 위한 객체 파일 생성

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const BUY_MESSAGE = {
+  BUY: "개를 구매했습니다.",
+};
+
+const LOTTO_PRICE = 1000;
+
+const RESULT_MESSAGE = {
+  FIRST: "6개 번호 일치 / 2,000,000,000원",
+  SECOND: "5개 번호 + 보너스 번호 일치 / 30,000,000원",
+  THIRD: "5개 번호 일치 / 1,500,000원",
+  FOURTH: "4개 번호 일치 / 50,000원",
+  FIFTH: "3개 번호 일치 / 5,000원",
+};
+
+module.exports = { BUY_MESSAGE, LOTTO_PRICE, RESULT_MESSAGE };
