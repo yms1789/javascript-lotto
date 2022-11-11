e60cbb916bd0cc6677f6f57bde9df32efd4eb853
YMS <yms1789@naver.com>
2022-11-11 16:14:06 +0900

    Feat: 로또 발행 기능 구현

--- src/Buy.js
+++ src/Buy.js
@@ -1,14 +1,21 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 const Lotto = require("./Lotto");
-const { BUY_MESSAGE } = require("./constants");
+const { BUY_MESSAGE, LOTTO_PRICE } = require("./constants");
 
 const messages = {};
+
 class Buy {
   print(message) {
     return Console.print(message);
   }
   printLottoCount(money) {
-    return this.print(`${Math.floor(money / 1000)} ${BUY_MESSAGE.BUY}`);
+    return this.print(`${Math.floor(money / LOTTO_PRICE)} ${BUY_MESSAGE.BUY}`);
+  }
+  lottoPublish(money) {
+    const buyLottoCount = Math.floor(money / LOTTO_PRICE);
+    for (let i = 0; i < buyLottoCount; i++){
+      printLottoDetails();
+    }
   }
   input() {
     Console.readLine("로또 구입 금액을 입력하세요.\n", (money) => {
