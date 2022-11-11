f271e0da09a45cb30f912b0b432fb308819ba24c
YMS <yms1789@naver.com>
2022-11-11 16:56:44 +0900

    Feat: 로또 번호 예외 처리 사항 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,4 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
 class Lotto {
   #numbers;
 
@@ -5,14 +6,16 @@ class Lotto {
     this.validate(numbers);
     this.#numbers = numbers;
   }
-
+  
   validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    numbers.forEach((number) => {
+      if (number > 45 || number < 1) {
+        throw new Error("[ERROR] 로또 번호의 숫자 범위는 1 ~ 45까지 입니다.");
+      }
+    });
   }
-
-  // TODO: 추가 기능 구현
 }
-
 module.exports = Lotto;
