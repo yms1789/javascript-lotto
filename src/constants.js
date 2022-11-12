6c8ca0f7d48bf5e73c1b6a1a4d6de7f73f8cbc54
YMS <yms1789@naver.com>
2022-11-12 18:21:44 +0900

    Feat: 출력 메세지 및 로또 금액 상수 객체 생성

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,23 @@
-const BUY_MESSAGE = {
+const BUY_MESSAGE = Object.freeze({
   BUY: "개를 구매했습니다.",
-};
+});
 
 const LOTTO_PRICE = 1000;
 
-const RESULT_MESSAGE = {
-  FIRST: "6개 번호 일치 / 2,000,000,000원",
-  SECOND: "5개 번호 + 보너스 번호 일치 / 30,000,000원",
-  THIRD: "5개 번호 일치 / 1,500,000원",
-  FOURTH: "4개 번호 일치 / 50,000원",
-  FIFTH: "3개 번호 일치 / 5,000원",
-};
+const RESULT_MESSAGE = Object.freeze({
+  1: "6개 일치 (2,000,000,000원)",
+  2: "5개 일치, 보너스 볼 일치 (30,000,000원)",
+  3: "5개 일치 (1,500,000원)",
+  4: "4개 일치 (50,000원)",
+  5: "3개 일치 (5,000원)",
+});
 
-module.exports = { BUY_MESSAGE, LOTTO_PRICE, RESULT_MESSAGE };
+const WINNING_AMOUNT = Object.freeze({
+  1: 200000000,
+  2: 30000000,
+  3: 1500000,
+  4: 50000,
+  5: 5000,
+});
+
+module.exports = { BUY_MESSAGE, LOTTO_PRICE, RESULT_MESSAGE, WINNING_AMOUNT };
